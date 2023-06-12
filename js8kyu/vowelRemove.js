b788e59efdb16eed41f65988cbab952b96d050d6
SabertoothSlug <developerdiehl@gmail.com>
2023-06-12 19:54:56 -0400

    codewars 06/12/2023

---
+++ js8kyu/vowelRemove.js
@@ -0,0 +1,9 @@
+function shortcut (string) {
+    let arr=string.split('')
+    let arr2=arr.map((element,i)=>{
+        if(element!='a'&&element!='e'&&element!='i'&&element!='o'&&element!='u'){
+            return element
+        }
+    })
+    return arr2.join('');
+  }
