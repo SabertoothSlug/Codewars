ef2c3e744be5f6d38706e9ab03212380a2720e38
SabertoothSlug <developerdiehl@gmail.com>
2023-07-06 22:00:49 -0400

    codewars 07/06/2023

---
+++ js6kyu/breakCamelCase.js
@@ -0,0 +1,13 @@
+// complete the function
+function solution(string) {
+let regex=/[a-z][A-Z]/
+while(regex.test(string)){
+    let i=string.search(regex)
+    console.log(string.search(regex))
+    string=string.slice(0,i+1)+' '+string.slice(i+1,string.length+1)
+}
+return string
+
+
+}
+console.log(solution('camelCasingTest'))
