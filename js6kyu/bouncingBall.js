e967b2664b02835bbbce2766092776a714ae9943
SabertoothSlug <developerdiehl@gmail.com>
2023-07-07 19:23:34 -0400

    codewars 07/07/2023

---
+++ js6kyu/bouncingBall.js
@@ -0,0 +1,13 @@
+function bouncingBall(h,  bounce,  window) {
+    if(h>0&&bounce>0&&bounce<1&&window<h){
+        let view=1
+        let transition=bounce*h
+        while(transition>=window){
+            transition*=bounce
+            view+=2
+        }
+        return view
+    }else{
+        return -1
+    }
+  }
