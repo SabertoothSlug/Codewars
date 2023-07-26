70cfbcf666edf658720a2bb7ba752f9c872f8039
SabertoothSlug <developerdiehl@gmail.com>
2023-07-26 14:40:09 -0400

    codewars 07/26/2023 part 2

--- js6kyu/tribonacci.js
+++ js6kyu/tribonacci.js
@@ -1,12 +1,27 @@
 function tribonacci(signature,n){
     let seq=signature
-    if(n===0){
-        return []
+    let indy=0
+    if(n<=3){
+        while(seq.length>n){
+            seq.pop()
+        }
+        return seq
     }else{
         while(seq.length<n){
-            console.log(seq)
-            seq.push((Number(seq[-1])+Number(seq[-2])+Number(seq[-3])))
+            let sum=0
+            for(i=0;i<3;i++){
+                sum+=seq[indy+i]
+            }
+            seq.push(sum)
+            indy++
         }
         return seq
     }
+  }
+
+  function tribonacci(signature,n){  
+    for (var i = 0; i < n-3; i++) { // iterate n times
+      signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
+    }
+    return signature.slice(0, n); //return trib - length of n
   }
