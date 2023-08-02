5c638e874d8c65af548e513ccc97771bcd7e6a29
SabertoothSlug <developerdiehl@gmail.com>
2023-08-02 17:13:58 -0400

    codewars 08/02/2023

---
+++ js8kyu/alternatingCase.js
@@ -0,0 +1,18 @@
+String.prototype.toAlternatingCase = function () {
+    const regex1=/[A-Z]/
+    const regex2=/[a-z]/
+    let array=this.split('').forEach(element=>{
+        if(regex1.test(element)){
+            return element.toLowerCase()
+        }else if(regex2.test(element)){
+            return element.toUpperCase()               
+        }else {
+            return element
+        }
+    })
+    return array.join('')
+  }
+
+  String.prototype.toAlternatingCase = function () {
+    return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
+}
