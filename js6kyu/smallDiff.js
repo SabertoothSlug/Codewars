e41ebab9cfdd442e5a13ecb9a55635bdb21328ec
SabertoothSlug <developerdiehl@gmail.com>
2023-08-10 15:54:02 -0400

    codewars 08/10/2023

--- js6kyu/smallDiff.js
+++ js6kyu/smallDiff.js
@@ -7,38 +7,92 @@ function oneCharDifference(s1, s2) {
     for(let i=0;i<arr1.length;i++){
      
         if(count==0&&arr1[i]!==arr2[v]){
+           if(arr1[i]==arr2[v-1]){
+            v--
+            vcount++
             count++
-            console.log(arr1[i])
-            
-        }
-        if(count==1&&arr1[i]!==arr2[v]&&vcount>0){
+            console.log('1')
+           }else if(arr1[i]==arr2[v+1]){
+            v++
+            vcount++
             count++
-            console.log(arr1[i])
+            console.log('2')
+           }else{
+            count++
+            console.log('3')
+           }
             
-        }
-        if(count==1&&arr1[i]!==arr2[v]&&vcount==0){
+        }else if(count==1&&arr1[i]!==arr2[v]&&vcount>0){
+            count++
+            console.log('4')
+            
+        }else if(count==1&&arr1[i]!==arr2[v]&&vcount==0){
             if(arr1[i]==arr2[v-1]){
                 v--
                 vcount++
-            }
-            if(arr1[i]==arr2[v+1])
-            v++
+                
+                console.log('5')
+            }else if(arr1[i]==arr2[v+1]){
+                v++
             vcount++
+            
+            console.log('6')
+            }else{
+                count++
+                console.log('7')
+            }
+            
         }
        
         v++
     }
-    if (arr1.length!=arr2.length){
+    if(arr1.length==arr2.length){
+        if(arr1[0]==arr2[0]&&arr2[arr2.length-1]!=arr1[arr1.length-1]){
+            count++
+        }
+    }
+    if (arr1.length<arr2.length){
+       
+
+        if(arr2[arr2.length-1]!=arr1[arr1.length-1]){
+            count++
+        }
+    }
+    if(!s1&&s2){
         count++
     }    
     if(count==1){
-        return `${s1} and ${s2} differ by one letter`
+        console.log(`${s1} and ${s2} differ by one letter`)
+        return true
     }else if(count==0){
-        return `same string`
+        console.log(`same string`)
+        return false
     }else{
-        return `${s1} and ${s2} differ by more than one letter`
+        console.log(`${s1} and ${s2} differ by more than one letter`)
+        return false
     }
 }
 
-console.log(oneCharDifference('grant', 'grannt'))
+console.log(oneCharDifference('grant', 'gant'))
 
+function oneCharDifference(a, b) {
+    let  m = a.length
+    ,    n = b.length
+    , diff = 0;
+    
+    for (let i = 0, j = 0; i < m || j < n; i++, j++) {
+      
+      if (a[i] === b[j])
+        continue;
+      
+      diff++;
+      
+      if(a[i + 1] === b[j])
+        j--;
+        
+      else if(a[i] === b[j + 1])
+        i--;
+    }
+    
+    return diff === 1;
+  }
